0c194d47f257e5b0eef1c19541b1615b0b5519b8
romingoon <romingoon@gmail.com>
2025-11-12 13:35:21 +0900

    영락교회 위경도 좌표 수정

--- src/components/pages/LocationPage.tsx
+++ src/components/pages/LocationPage.tsx
@@ -44,8 +44,8 @@ export function LocationPage({ venue, venueAddress }: LocationPageProps) {
 
   // 영락교회 좌표
   const CHURCH_COORDS = {
-    lat: 37.564243536671,
-    lng: 126.98999850251,
+    lat: 37.5649,
+    lng: 126.9917,
   };
 
   // 사용자 현재 위치 가져오기
