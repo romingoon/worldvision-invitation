23adb6bc5a1e3207e3f76be48eddaa21738b9f3f
romingoon <romingoon@gmail.com>
2025-11-12 18:44:49 +0900

    폰트 추가 126

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko" className="font-NanumBarun">
+    <html lang="ko" className={`${NanumBarun.variable}`}>
       <head></head>
       <body suppressHydrationWarning>
         <ImagePreloader />
