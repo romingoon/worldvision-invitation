364648d0c96a5e5919cadb929dbe6bf137755808
romingoon <romingoon@gmail.com>
2025-11-12 17:34:57 +0900

    폰트.ts 추가22

--- public/fonts/fonts.ts
+++ public/fonts/fonts.ts
@@ -3,17 +3,17 @@ import localFont from 'next/font/local';
 export const NanumBarunGothic = localFont({
   src: [
     {
-      path: '../public/fonts/NanumBarunGothic.woff2',
+      path: './NanumBarunGothic.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/NanumBarunGothicRegular.woff2',
+      path: './NanumBarunGothicRegular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/NanumBarunGothicBold.woff2',
+      path: './NanumBarunGothicBold.woff2',
       weight: '700',
       style: 'normal',
     },
